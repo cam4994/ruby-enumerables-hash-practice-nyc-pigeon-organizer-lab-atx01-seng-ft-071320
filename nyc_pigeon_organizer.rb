d67ecc6f3b7897fe6b43a0f3f79e0db8077553f7
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-28 22:51:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,13 +7,13 @@ def nyc_pigeon_organizer(data)
     first_value.each do |key, array|
       array.each do |name|
         if unique_name.include?(name) == false
-          new_hash[name.to_sym] = {}
+          new_hash[name] = {}
           unique_name.push(name)
         end 
-        if new_hash[name.to_sym][first_key] == nil 
-          new_hash[name.to_sym][first_key] = []
+        if new_hash[name][first_key] == nil 
+          new_hash[name][first_key] = []
         end 
-        new_hash[name.to_sym][first_key].push(key.to_s)
+        new_hash[name[first_key].push(key.to_s)
       end
     end 
   end 
