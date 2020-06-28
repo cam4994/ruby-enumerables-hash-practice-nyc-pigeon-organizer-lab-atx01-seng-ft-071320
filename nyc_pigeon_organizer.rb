7e4417600d8fae43cc2e4bf51b9031ef40a6cca5
Colin Mosley <colin.a.mosley@gmail.com>
2020-06-28 22:02:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {} 
+  unique_name = []
+  data.each do |first_key, first_value|
+    first_value.each do |key, array|
+      array.each do |name|
+        if unique_name.include?(name) == false
+          new_hash[name.to_sym] = {}
+          unique_name.push(name)
+          new_hash[name.to_sym][first_key] = []
+          new_hash[name.to_sym][first_key].push(key.to_s)
+        else 
+          new_hash[name.to_sym][first_key] = []
+          new_hash[name.to_sym][first_key].push(key.to_s)
+        end
+      end 
+      binding.pry
+    end 
+  end 
+  new_hash
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+nyc_pigeon_organizer(pigeon_data)
